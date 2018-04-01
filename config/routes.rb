c2afc95595489466ac1c46cd0a0164f071c44750
ex-liyue003 <ex-liyue003@pingan.com.cn>
2018-04-01 22:30:37 +0800

    add contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  root'static_page#home'
+
   get 'static_page/home'
-
   get 'static_page/help'
-
   get 'static_page/about'
-
-  get  'static_page/contact'
-  root 'application#hello'
+  get 'static_page/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
